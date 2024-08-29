a275857d52de19d89899db660eee0a63b562400c
NsikanPatrick <nsikanpatrick69@gmail.com>
2024-08-29 18:23:05 +0200

    Alternating env variables 2

--- src/firebase.js
+++ src/firebase.js
@@ -6,22 +6,13 @@ import {getFirestore} from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_API_KEY,
-//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDFXYlCsVGPn9Zpz2xG-vq7dmeK0a79EHQ",
-  authDomain: "todo2-doist.firebaseapp.com",
-  projectId: "todo2-doist",
-  storageBucket: "todo2-doist.appspot.com",
-  messagingSenderId: "215709524804",
-  appId: "1:215709524804:web:5e5bac6d2ffa8623e66d11"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
